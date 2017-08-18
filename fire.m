d24e7d1e26f842acac2a013f8a72e9742e72e96a
Rodney Polkinghorne <rpolkinghorne@swin.edu.au>
2017-08-18 15:57:12 +1000

    save the version of the fire script before it was generalised

--- fire.m
+++ fire.m
@@ -1,7 +1,40 @@
 function fire(letter, queue)
 % FIRE  start oscor running on g2
 %
-%    FIRE(NAME, QUEUE) submits a job QUEUE to run in directory ~/NAME
+%    FIRE(COMMAND, NAME, QUEUE) copies files from the current directory to
+%    ~/NAME on the Green II cluster, then runs the Matlab script or
+%    function COMMAND in that directory, using the specified QUEUE.
+%    
+%    This requires that ssh configured to login without any interaction.
+%    Your cluster login is defined in a file parameters.m, which you will
+%    need to edit. You will also need to edit a few other parameters,
+%    including the names of the files to copy to the cluster.
+%    
+%    This is intended for use with XSPDE jobs, and it has some features to
+%    assist with running them in parallel. The files parameters.m and
+%    ensembles.m have a special meaning.
+%    
+%    The script parameters.m should define a set of XSPDE input structures,
+%    or at least the points, steps and ensembles fields of those
+%    structures. (You can set up the structures elsewhere, then run
+%    parameters to assign these fields.) It should also list the names of
+%    these structures in a cell array called fire_inputs.
+%    
+%    One of the parameters defined is hours_per_step. Run a small job to
+%    start, then set this in parameters.m, so that fire will automatically
+%    request an appropriate wall time.
+%    
+%    The fire script generates a batch script, and also a script
+%    ensembles.m. The latter assigns ensembles fields to the input
+%    structures in fire_inputs, so that each input structure has the same
+%    number of trajectories as requested, but with parallelism appropriate
+%    to QUEUE. Your parameters.m script should run this as its last step.
+%    The idea is to write parameters.m to run on your desktop machine, and
+%    let the generated ensembles script reconfigure it for the cluster. The
+%    job control script will request parallel Matlab resources consistent
+%    with the ensembles it generates.
+
+
 
 switch queue
 	case 'gstar', cpus = 10;
@@ -49,7 +82,8 @@ fclose(ensfile);
 
 runfile = fopen(['~/Desktop/gstar/' letter '/runjob'], 'w');
 fprintf(runfile, '#!/bin/sh\n#PBS -q %s\n', queue);
-fprintf(runfile, '#PBS -l nodes=1:ppn=%d\n#PBS -l walltime=%d:00:00\n', cpus, ceil(work/1e9));
+fprintf(runfile, '#PBS -l nodes=1:ppn=%d\n', cpus);
+fprintf(runfile, '#PBS -l walltime=%d:00:00\n#PBS -N %s\n', ceil(2*work/1e9), letter);
 fprintf(runfile, ['module load matlab/R2015b\ncd %s\n' ...
 	'echo "Working directory: %s"\n' ...
 	'matlab -r ''parpool(%d); xpsetup; oscor; quit''\n'], ...
